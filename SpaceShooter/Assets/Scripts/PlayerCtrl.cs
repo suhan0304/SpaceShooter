a383d34cfa2bd1002064b58f6b24be1b8c94b622
ZerrY <suhan0304@gmail.com>
2024-06-17 19:18:49 +0900

    Import Animation

--- SpaceShooter/Assets/Scripts/PlayerCtrl.cs
+++ SpaceShooter/Assets/Scripts/PlayerCtrl.cs
@@ -6,6 +6,7 @@ public class PlayerCtrl : MonoBehaviour
 {
     private Transform tr;
     public float moveSpeed = 10f;
+    public float turnSpeed = 80f;
     void Start()
     {
         tr = GetComponent<Transform>();
@@ -15,10 +16,12 @@ public class PlayerCtrl : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+        float r = Input.GetAxis("Mouse X");
 
         Vector3 moveDir = (Vector3.forward* v) + (Vector3.right) * h;
 
         tr.Translate(moveDir.normalized * moveSpeed * Time.deltaTime);
 
+        tr.Rotate(Vector3.up * turnSpeed * Time.deltaTime * r);
     }
 }
