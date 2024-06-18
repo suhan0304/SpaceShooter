fd8ffe0b846ffbc663f9c7e68b45e3434231f402
ZerrY <suhan0304@gmail.com>
2024-06-18 14:02:47 +0900

    RemoveBullet Update

---
+++ SpaceShooter/Assets/Scripts/RemoveBullet.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveBullet : MonoBehaviour
+{
+    void OnCollisionEnter(Collision coll) {
+         if (coll.collider.tag == "BULLET") {
+            Destroy(coll.gameObject);
+         }       
+    }
+}
