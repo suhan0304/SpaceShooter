69f432cbd9996d67204fc3d510ab2c7832251629
ZerrY <suhan0304@gmail.com>
2024-06-18 14:49:40 +0900

    Re Import Particle

--- SpaceShooter/Assets/Scripts/RemoveBullet.cs
+++ SpaceShooter/Assets/Scripts/RemoveBullet.cs
@@ -7,8 +7,13 @@ public class RemoveBullet : MonoBehaviour
     public GameObject sparkEffect;
     void OnCollisionEnter(Collision coll) {
          if (coll.collider.CompareTag("BULLET")) {
+            ContactPoint cp = coll.GetContact(0);
 
-            Instantiate(sparkEffect, coll.transform.position, Quaternion.identity);
+            Quaternion rot = Quaternion.LookRotation(-cp.normal);
+
+            GameObject spark = Instantiate(sparkEffect, cp.point, rot);
+
+            Destroy(spark, 0.5f);
 
             Destroy(coll.gameObject);
          }       
