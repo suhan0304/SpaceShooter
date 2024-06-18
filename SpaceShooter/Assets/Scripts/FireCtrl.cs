181a48747b66db6ea48414802bcf8a070445cffd
ZerrY <suhan0304@gmail.com>
2024-06-18 14:13:00 +0900

    Update Fire Bullet

---
+++ SpaceShooter/Assets/Scripts/FireCtrl.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireCtrl : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform firePos;
+    
+     void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0)) {
+            Fire();
+        }
+    }
+
+    void Fire() {
+        Instantiate(bullet, firePos.position, firePos.rotation);
+    }
+}
