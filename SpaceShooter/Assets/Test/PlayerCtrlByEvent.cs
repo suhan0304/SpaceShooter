0be9e4abb8feb805e487c520f7603dae1b70734b
ZerrY <suhan0304@gmail.com>
2024-06-20 12:06:11 +0900

    Update PlayerCtrlByEvent.cs

--- SpaceShooter/Assets/Test/PlayerCtrlByEvent.cs
+++ SpaceShooter/Assets/Test/PlayerCtrlByEvent.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class PlayerCtrlByEvent : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private InputAction moveAction;
+    private InputAction attackAction;
+
+    private Animator anim;
+    private Vector3 moveDir;
+
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+
+        moveAction = new InputAction("Move", InputActionType.Value);
+
+        moveAction.AddCompositeBinding("2DVector")
+        .With("Up","<Keyboard>/w")
+        .With("Down","<Keyboard>/s")
+        .With("Left","<Keyboard>/a")
+        .With("Right","<Keyboard>/d");
+
+        moveAction.performed += ctx => {
+            Vector2 dir = ctx.ReadValue<Vector2>();
+            moveDir = new Vector3(dir.x, 0, dir.y);
+            anim.SetFloat("Movement", dir.magnitude);
+        };
+        moveAction.canceled += ctx => {
+            moveDir = Vector3.zero;
+            anim.SetFloat("Movement", 0.0f);
+        };
+        moveAction = new InputAction("Attack", InputActionType.Button, "<Keyboard>/space");
+
+        attackAction.performed += ctx => {
+            Debug.Log("Attack by c# event");
+            anim.SetTrigger("Attack");
+        };
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if (moveDir != Vector3.zero) {
+            transform.rotation = Quaternion.LookRotation(moveDir);
+            transform.Translate(Vector3.forward * Time.deltaTime * 4.0f);
+        }
     }
 }
