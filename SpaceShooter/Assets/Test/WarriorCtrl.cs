98422b9eaff0d377ab90d6e77c3f676171702487
ZerrY <suhan0304@gmail.com>
2024-06-20 11:53:16 +0900

    Update WarriorCtrl.cs

--- SpaceShooter/Assets/Test/WarriorCtrl.cs
+++ SpaceShooter/Assets/Test/WarriorCtrl.cs
@@ -21,6 +21,7 @@ public class WarriorCtrl : MonoBehaviour
         }
     }
 
+#region SEND_MESSAGE
     void OnMove(InputValue value) {
         Vector2 dir = value.Get<Vector2>();
 
@@ -34,4 +35,24 @@ public class WarriorCtrl : MonoBehaviour
         Debug.Log("Attack");
         anim.SetTrigger("Attack");
     }
+#endregion SEND_MESSAGE
+
+#region UNITY_EVENTS 
+    public void OnMove(InputAction.CallbackContext ctx) {
+        Vector2 dir = ctx.ReadValue<Vector2>();
+        moveDir = new Vector3 (dir.x, 0, dir.y);
+
+        anim.SetFloat("Movement", dir.magnitude);
+        Debug.Log($"Move = ({dir.x}) {dir.y}");
+    }
+    public void OnAttack(InputAction.CallbackContext ctx) {
+        Debug.Log($"ctx.phase={ctx.phase}");
+
+        if (ctx.performed) {
+            Debug.Log("Attack");
+            anim.SetTrigger("Attack");
+        }
+    }
+#endregion UNITY_EVENTS
 }
+
